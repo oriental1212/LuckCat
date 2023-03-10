c1943780cc19e9b3e74c8c434ec73ba02926b25d
Oriental <1304206691@qq.com>
2023-03-10 16:07:40 +0800

    项目初始化

---
+++ src/main/java/com/luckcat/LuckCatApplication.java
@@ -0,0 +1,13 @@
+package com.luckcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckCatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckcat/LuckCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
